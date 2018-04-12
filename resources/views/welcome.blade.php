03e0d8b2ae579af1d21c852be91e4790cc6093e7
Adam Engebretson <adam@enge.me>
2018-04-12 17:50:06 -0500

    Tidying

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,3 @@
 @section('content')
     <h1>This is the Home Page</h1>
 @endsection
-
-@section('scripts')
-<script>
-    $(function() {
-        alert('Hello and welcome to my home page')
-    })
-</script>
-@endsection
